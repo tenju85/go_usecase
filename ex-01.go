46a8850525403952ba190cddb0ee0bf56338f384
toyo8603 <toyo8603@gmail.com>
2024-02-21 21:14:40 +0900

    first commit

---
+++ ex-01.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"unicode/utf8"
+)
+
+func main() {
+	fmt.Println("Starting the program...")
+	now := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println(now)
+	emoji()
+}
+
+func emoji() {
+	msg := "Ho🌮あA"
+	println(msg, "(", utf8.RuneCountInString(msg), " chars ")
+	for i, cc := range msg {
+		fmt.Printf("code boundary:%d .. %c\n", i, cc)
+	}
+	println(len([]rune(msg)), len(msg))
+	rca := []rune(msg)
+	for i, cc := range rca {
+		fmt.Printf("(%d) .. %c\n", i, cc)
+	}
+	ss := []rune(rca[2:4])
+	fmt.Printf("%c %c %c\n", ss[0], ss, rca[2:4]) // slice of runes
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/ex-01
+
+go 1.22.0
