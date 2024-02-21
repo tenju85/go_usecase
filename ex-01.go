6166caf1dad5b5c0e9f1d2288d86157226722a97
toyo8603 <toyo8603@gmail.com>
2024-02-21 21:56:22 +0900

    correct date format.

--- ex-01.go
+++ ex-01.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	fmt.Println("Starting the program...")
-	now := time.Now().Format("2006年01月02日 15:04:05")
+	now := time.Now().Format("2006年01月02日 15時04分05秒")
 	fmt.Println(now)
 	emoji()
 }
