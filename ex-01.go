0b23ab3e76ef6fd01ef5ca02102bcf6f45dcb4fa
toyo8603 <toyo8603@gmail.com>
2024-02-22 16:37:34 +0900

    second commit

--- ex-01.go
+++ ex-01.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"regexp"
+	//"strconv"
+	"strings"
 	"time"
 	"unicode/utf8"
+
+	"github.com/samber/lo"
 )
 
 func main() {
@@ -11,6 +20,62 @@ func main() {
 	now := time.Now().Format("2006年01月02日 15時04分05秒")
 	fmt.Println(now)
 	emoji()
+	misc()
+	file_read()
+
+	ret := input("Enter your name > ")
+	fmt.Printf("your name is %s\n", ret)
+}
+
+func file_io() {
+	f, err := os.Open("go.mod")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func input(prompt string) string {
+	print(prompt)
+	buf, err := bufio.NewReader(os.Stdin).ReadBytes('\n') // 读取一行，包括换行符
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(buf)
+}
+
+var nonAlphanumericRegex = regexp.MustCompile(`[^\p{L}\p{N} ]+`)
+
+func clearString(str string) string {
+    return nonAlphanumericRegex.ReplaceAllString(str, "")
+}
+
+func regexp_01() {
+    str := "Test@%String本田🌮#321gosamples.dev ـا ą ٦"
+    fmt.Println(clearString(str))
+		
+		text := "GOSAMPLES.dev is  \t \r\n the best Golang \t\t\t\t    website in the \n world!"
+    re := regexp.MustCompile(`\s+`)
+    res := re.ReplaceAllString(text, " ")
+    println(text,"\n--->>\n", res)
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randomString(n int) string {
+    sb := strings.Builder{}
+    sb.Grow(n)
+    for i := 0; i < n; i++ {
+        sb.WriteByte(charset[rand.Intn(len(charset))])
+    }
+    return sb.String()
 }
 
 func emoji() {
@@ -26,4 +91,17 @@ func emoji() {
 	}
 	ss := []rune(rca[2:4])
 	fmt.Printf("%c %c %c\n", ss[0], ss, rca[2:4]) // slice of runes
+}
+func misc(){
+	matching := lo.FilterMap([]string{"cpu", "gpu", "mouse", "keyboard"}, func(x string, _ int) (string, bool) {
+    if strings.HasSuffix(x, "pu") {
+        return "xpu", true
+    }
+    return "", false
+})
+println(matching)
+// []string{"xpu", "xpu"}
+}
+func file_read(){
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module example.com/ex-01
 
 go 1.22.0
+
+require (
+	github.com/samber/lo v1.39.0
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+)
